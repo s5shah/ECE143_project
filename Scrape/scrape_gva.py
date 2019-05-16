98f6eb7dd676cf6258e17d604842cbf97703995e
Sneh Shah <s5shah@eng.ucsd.edu>
2019-05-16 13:31:04 -0700

    corrected program for scraping gva, added dataset

--- Scrape/scrape_gva.py
+++ Scrape/scrape_gva.py
@@ -8,48 +8,60 @@ import bs4
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 
-def scrape_gva(year):
+def scrape_gva(data):
     '''
     Input :-
-    year - type: int, range: 2014 - 2019 (inclusive)
+    data - type: str, len: 1, allowed characters: ['m', 'o']
+    
+    if data == m:
+        scrape the mass shootings data.
+    elif data == o:
+        scrape the officers killing data.
     
     Output :-
     {year}.csv file containing: Incident Date, State, City/County, #killed, #injured
     
     The function takes in a year as input and returns a csv file that contains the above mentioned data from
     "www.gunviolencearchive.org" website.
-    The data is specific to mass shootings throughout United States.
+    The data is specific to either mass shootings or shootings involving police officers throughout United States.
     
     To run this program, you need the following:
         1. selenium package to be installed.
         2. google chrome installed
         3. chromedriver path added to path variables.
     '''
-    assert isinstance(year, int)
-    assert year in range(2014, 2020)
+    assert isinstance(data, str)
+    assert len(data) == 1
+    assert data in ['m', 'o']
     
-    driver = webdriver.Chrome(options = chrome_options)
-    driver.get(f"https://www.gunviolencearchive.org/reports/mass-shooting?year={year}")
-    # https://www.gunviolencearchive.org/reports/officer-shot-killed?year={year}
-
-    stuff = []
-    x = True
-    while x:
-        #Scrape here    
-        soup = BeautifulSoup(driver.page_source)
-        links = soup.find_all('td')
-        for link in links:
-            if (type(link.contents[0]) == bs4.element.NavigableString):
-                stuff.append(link.contents[0])
-        #Scraping done
-        try:
-            element = driver.find_element_by_partial_link_text('next')
-            element.click()
-        except NoSuchElementException:
-            x = False
-            
-    driver.close()
+    for year in range(2014,2020):
+        if data == 'm':
+            url = f"https://www.gunviolencearchive.org/reports/mass-shooting?year={year}"
+            fname = f"Mass_Shootings_{year}.csv"
+        elif data == 'o':
+            url = f"https://www.gunviolencearchive.org/reports/officer-shot-killed?year={year}"
+            fname = f"Officers_Shootings_{year}.csv"
+        
+        driver = webdriver.Chrome(options = chrome_options)
+        driver.get(url)
     
-    #Stuff ready here. Write to CSV after using pandas
-    df = pd.DataFrame(np.array(stuff).reshape(int((len(stuff))/6), 6), columns = ['Incident Date', 'State', 'City/County', 'Address', '# killed', '# injured'])
-    df.to_csv(f'{year}.csv', encoding='utf-8', index=False)
+        stuff = []
+        while True:
+            #Scrape here    
+            soup = BeautifulSoup(driver.page_source)
+            scrollable = soup.find('table', {'class':"sticky-enabled tableheader-processed sticky-table"})
+            links = scrollable.findAll('td')
+            for link in links:
+                if (type(link.contents[0]) == bs4.element.NavigableString):
+                    stuff.append(link.contents[0])
+            #Scraping done
+            try:
+                element = driver.find_element_by_partial_link_text('next')
+                element.click()
+            except NoSuchElementException:
+                break
+        driver.close()
+        
+        #Stuff ready here. Write to CSV after using pandas
+        df = pd.DataFrame(np.array(stuff).reshape(int((len(stuff))/6), 6), columns = ['Incident Date', 'State', 'City/County', 'Address', '# killed', '# injured'])
+        df.to_csv(fname, encoding='utf-8', index=False)
