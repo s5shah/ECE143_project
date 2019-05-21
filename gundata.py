47e40d0693c5ac53150a41e5e4174ec7e23a903b
like3107 <519486207@qq.com>
2019-05-21 00:52:43 -0700

    Add files via upload

--- gundata.py
+++ gundata.py
@@ -6,10 +6,12 @@ class gundata:
 	'''
 	A class which defines several functions to deal with gun violence data(mass shooting) 
 	'''
-	def __init__(self,year):
+	def __init__(self,datatype,year):
 		assert isinstance(year,int),"invalid year"
-		assert 2014<=year<=2018,"Only 2014-2018 mass shooting data available"
-		self.data = pd.read_csv(f"dataset/ms{year}.csv")
+		assert isinstance(datatype,str),"datatype should be Mass_shootings,Officers_Shootings,School_Schootings"
+		assert 2014<=year<=2019,"Only 2014-2019 mass shooting data available"
+		self.datatype = datatype
+		self.data = pd.read_csv(f"Scrape/{datatype}_{year}.csv")
 		self.monthlist = {12:'Dec',11:'Nov',10:'Oct',9:'Sep',8:"Aug",7:"Jul",6:"Jun",5:"May",4:"Apr",3:"Mar",2:"Feb",1:"Jan"}
 
 	def get_total_insidence(self,month=None):
@@ -26,14 +28,20 @@ class gundata:
 
 		if isinstance(month,int):
 			assert 1<=month<=12
-			return len(data.loc[self.data['Incident Date'].str.contains(self.monthlist[month])])
+			if self.datatype == 'School_Schootings':
+				return sum(self.data['Date'].str.split('/').str[0]==str(month))
+			else:
+				return len(data.loc[self.data['Date'].str.contains(self.monthlist[month])])
 
 		if isinstance(month,list) or isinstance(month,tuple):
 			assert len(month)==2,"invalid input"
 			assert 1<=month[0]<= 12 and 1<=month[1]<=12 and month[0]<month[1],"invalid input"
 			res = []
 			for i in range(month[0],month[1]+1):
-				res.append(len(self.data.loc[self.data['Incident Date'].str.contains(self.monthlist[i])]))
+				if self.datatype == 'School_Schootings':
+					res.append(sum(self.data['Date'].str.split('/').str[0]==str(i)))
+				else:
+					res.append(len(self.data.loc[self.data['Incident Date'].str.contains(self.monthlist[i])]))
 			return res
 
 	#To be updated,sum
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 #project ECE143
 import matplotlib.pyplot as plt
+import numpy as np
 from gundata import gundata
 
 class data:
-	def __init__(self):
-		self.data2014 = gundata(2014)
-		self.data2015 = gundata(2015)
-		self.data2016 = gundata(2016)
-		self.data2017 = gundata(2017)
-		self.data2018 = gundata(2018)
+	def __init__(self,datatype):
+		self.datatype = datatype
+		self.data2014 = gundata(datatype,2014)
+		self.data2015 = gundata(datatype,2015)
+		self.data2016 = gundata(datatype,2016)
+		self.data2017 = gundata(datatype,2017)
+		self.data2018 = gundata(datatype,2018)
+		self.data2019 = gundata(datatype,2019)
 
 
 	def plot_incidence_per_year(self):
@@ -18,31 +21,46 @@ class data:
 		x2016 = self.data2016.get_total_insidence()
 		x2017 = self.data2017.get_total_insidence()
 		x2018 = self.data2018.get_total_insidence()
+		x2019 = self.data2019.get_total_insidence()
 		plt.plot(['2014','2015','2016','2017','2018'],[x2014,x2015,x2016,x2017,x2018])
 		plt.xlabel('Year') # apply labels
-		plt.ylabel('Nunber of mass shooting')
-		plt.title('Number of mass shooting per year')
+		plt.ylabel(f'Nunber of {self.datatype}')
+		plt.title(f'Number of {self.datatype} per year(2014-2018)')
 		plt.show()
 	def plot_incidence_per_month(self):
 		from matplotlib.pylab import subplots
 		fig,ax=subplots()
 		x = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-		x2014 = self.data2014.get_total_insidence([1,12])
-		x2015 = self.data2015.get_total_insidence([1,12])
-		x2016 = self.data2016.get_total_insidence([1,12])
-		x2017 = self.data2017.get_total_insidence([1,12])
-		x2018 = self.data2018.get_total_insidence([1,12])
-		ax.plot(x,x2014,x,x2015,x,x2016,x,x2017,x,x2018,'or--')
-		ax.title('Number of mass shooting per month(2014-2018)')
-		ax.legend(('2014','2015','2016','2017','2018'),loc='best')
+		x2014 = np.array(self.data2014.get_total_insidence([1,12]))
+		x2015 = np.array(self.data2015.get_total_insidence([1,12]))
+		x2016 = np.array(self.data2016.get_total_insidence([1,12]))
+		x2017 = np.array(self.data2017.get_total_insidence([1,12]))
+		x2018 = np.array(self.data2018.get_total_insidence([1,12]))
+		x2019 = np.array(self.data2019.get_total_insidence([1,12]))
+		ave = (x2014+x2015+x2016+x2017+x2018)/5
+		ax.plot(x,x2014,x,x2015,x,x2016,x,x2017,x,x2018,x,x2019,x,ave,'--or')
+		plt.title(f'Number of {self.datatype} per month(2014-2019.5.14)')
+		ax.legend(('2014','2015','2016','2017','2018','2019','ave'),loc='best')
+		plt.show()
+
+
 
 
 
 
 
 if __name__=="__main__":
-	data1 = data()
+	data1 = data("Mass_Shootings")
 	data1.plot_incidence_per_year()
 	data1.plot_incidence_per_month()
+
+	data2 = data("Officers_Shootings")
+	data2.plot_incidence_per_year()
+	data2.plot_incidence_per_month()
+
+
+	data3 = data("School_Schootings")
+	data3.plot_incidence_per_year()
+	data3.plot_incidence_per_month()
 	
 
